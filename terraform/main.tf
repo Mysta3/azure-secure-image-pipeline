67893226e86e03d198168155ea569136e9fa373b
Mysta3 <myshawnestallings@gmail.com>
2026-04-17 23:40:52 -0700

    added new definition type for image being added to sig

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,28 @@ resource "azurerm_resource_group" "main" { # rm - resource manager, azurerm - az
   tags     = var.tags
 }
 
+# define shared image gallery resource
 resource "azurerm_shared_image_gallery" "main" {
   name                = "sigsecureimagelab" # sig = shared image gallery
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   description         = "Gallery for secure images"
   tags                = var.tags
+}
+
+# define image type to publish to the gallery
+resource "azurerm_shared_image" "ubuntu_hardened" {
+  name                = "ubuntu-hardened"
+  gallery_name        = azurerm_shared_image_gallery.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "bic-proj"
+    offer     = "secure-ubuntu"
+    sku       = "baseline"
+  }
+
+  tags = var.tags
 }
